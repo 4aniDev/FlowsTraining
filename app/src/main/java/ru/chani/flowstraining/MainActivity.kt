4d88cef531b0311be0a2f0873315aa439d244da5
Dan <105542989+4aniDev@users.noreply.github.com>
2022-10-23 11:53:41 +0500

    asFlow

--- app/src/main/java/ru/chani/flowstraining/MainActivity.kt
+++ app/src/main/java/ru/chani/flowstraining/MainActivity.kt
@@ -2,10 +2,45 @@ package ru.chani.flowstraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        flowTraining()
+    }
+
+    fun flowTraining() {
+        val list = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        val flow = list.asFlow()
+
+        flow
+            .filter { it % 2 == 0 }
+            .map { it * 100 }
+
+
+        GlobalScope.launch {
+            flow.collect {
+                Log.d("TAG_MAIN", it.toString())
+            }
+        }
+
+        MainScope().launch {
+            flow
+                .filter { it == 6 }
+                .collect {
+                    Log.d("TAG_MAIN", it.toString())
+                }
+        }
     }
 }
